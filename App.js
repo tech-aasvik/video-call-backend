2273f2d29a0c649a2e38665c76e5268262b90673
Techaasvik <techaasvik@gmail.com>
2025-07-09 23:41:56 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Call from './Call';
+
+function App() {
+  return (
+    <div>
+      <h1>Video Calling App</h1>
+      <Call />
+    </div>
+  );
+}
+
+export default App;
